86b21d3c2d7f979b36cad21c42123b309b653f4a
ayishasharia <76717037+ayishasharia@users.noreply.github.com>
2021-03-11 23:32:26 +0530

    Add files via upload

---
+++ myphp/EXPERIMENT-14(student names).php
@@ -0,0 +1,23 @@
+﻿<?php
+echo "<h3>";
+$a="<h3 style='color:green'>";
+$b="<h3 style='color:pink'>";
+$c="</h3>";
+$name=["adi","viji","laya","reema","kailash"];
+$t=$name;
+echo "$a Printing using print_r $c";
+print_r($name);
+echo "<br>";
+echo "$b--------------------------------------------------------------------$c";
+echo "<br>";
+asort($name);
+echo "$a Sorting using asort() $c";
+print_r($name);
+echo "<br>";
+echo "$b-------------------------------------------------------------------$c";
+echo "<br>";
+arsort($t);
+echo "$a Sorting using arsort() $c";
+print_r($t);
+echo "</h3>";
+?>
---
+++ myphp/EXPERIMENT-15(Cricket players name).php
@@ -0,0 +1,37 @@
+﻿<html>
+<center>
+
+<body bgcolor="pink">
+
+<br>
+
+<?php
+
+
+$name=["Virat kohli","Rohit sharma","shikar Dhawan","KL Rahul","Rishab pant","Hardic pandya","R jadeja","R Ashwin","B kumar","J Bhumrha","m shami"];
+
+
+
+echo "<h4> <u><b>CRICKET TEAM PLAYERERS NAME</b></u></h4>
+<table border='1px'>
+ <tr><th class='h' >Sl No</th>
+<th class='h'>NAME</th>
+</tr>";
+
+for($i=0;$i<11;$i++)
+
+{
+
+$sl=$i+1;
+
+echo " <tr><th>$sl</th><th>$name[$i]</th></tr>";
+
+}
+
+echo "</table>";
+
+?>
+
+</body>
+
+</html>
